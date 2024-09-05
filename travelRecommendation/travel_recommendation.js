72f5c9ee73b4bce00786b6b01ac2a7162ff1de0a
Edith <edith-friedgund-johanna.foerster@stud.uni-bamberg.de>
2024-09-05 17:32:58 +0200

    see todos in .js

---
+++ travelRecommendation/travel_recommendation.js
@@ -0,0 +1,49 @@
+
+// task 6: fetch data
+// fetch data from the travel_recommendation_api.json file using the fetch API method, 
+// from there you can fetch travel-related details, such as the name of the place. 
+// You need to have your own images for every imageUrl in the JSON file.
+
+// >> see healthcare website
+
+// Fetch the data from the JSON using the fetch API() method. 
+// To check if you can access the data, you use console.log to see if the result is displayed.
+
+// It is good if the console.log logs the data. Otherwise, you need to look for a different API.
+
+
+
+// task 7: keyword searches 
+// see also healthcare app.
+//  also accept variants
+
+// For example, if the user enters "beach," or "beaches," "Beach" or "BEACH," 
+// then you need to write JavaScript code so that it accepts all variations of this keyword.
+
+// For uppercase letters in the keyword, you can convert them to lowercase 
+// in your JavaScript using the string manipulation toLowerCase() method.
+
+// Similarly, you need to create logic to match keywords entered for temples and countries.
+// its not expected the website can deal with anything else than beach, temple or countries.
+
+// The website should display results only after the user clicks the Search button.> onlick event handler
+
+// task 8: recommendations
+// In this task, you need to fetch the details of the places you recommend based on
+//  which keyword the user enters: beach, temple, or country.
+
+// For each of these three keywords, your results should display at least two recommendations, an image, and a description. 
+// these get rendered to the home page
+
+
+// task 9: clear button
+// Create logic in your JavaScript file for a clear button to clear the results. 
+// To implement this feature, you can create a function that will be called after clicking on the clear button in the navbar.
+
+
+
+// finally, check the output by cloning the repo into the working environment and running it on server.
+// do not forget the git part of it.
+
+
+
